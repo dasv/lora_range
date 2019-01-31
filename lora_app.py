e565aa3c20ab431fc40ef3dbba87efa25dac401c
dasv <danielsanv@gmail.com>
2019-01-31 15:37:42 +0100

    comentado

--- lora_app.py
+++ lora_app.py
@@ -1,33 +1,34 @@
 
-from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, copy_current_request_context, jsonify
+from flask_socketio import SocketIO, emit #Para manejar el socket
+from flask import Flask, render_template, url_for, copy_current_request_context, jsonify #Flask es un framework web ligero, se usa para servir las páginas
 import time
 from threading import Thread, Event
-from flask_sqlalchemy import SQLAlchemy
-from SX127x.LoRa import *
+from flask_sqlalchemy import SQLAlchemy #Manejo de base de datos, usamos sqlite
+from SX127x.LoRa import * #Manejo del módem
 from SX127x.board_config import BOARD
 import random
 import os
-import struct
+import struct #Para descodificar los mensajes recibidos
+import atexit
 
-import eventlet
+import eventlet #Sin esto no es posible conectar a los sockets en la raspberry
 eventlet.monkey_patch()
 
 
-
+#Configuraciones varias
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "points.db"))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-app.config['SECRET_KEY'] = 'ordidro'
+app.config['SECRET_KEY'] = 'wsnwsnwsn'
 app.config['DEBUG'] = True
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) #Instancia del objeto de la DB
 
-#BOARD.setup()
-#BOARD.reset()
+BOARD.setup()
+BOARD.reset()
 
 
 #turn the flask app into a socketio app
@@ -38,33 +39,16 @@ thread = Thread()
 thread_stop_event = Event()
 
 """
-class RandomThread(Thread):
-    def __init__(self):
-        self.delay = 1
-        super(RandomThread, self).__init__()
-
-    def randomNumberGenerator(self):
-        #infinite loop of magical random numbers
-        print("Making random numbers")
-        while not thread_stop_event.isSet():
-            latLon = [0, 0]
-            latLon[0] = random.uniform(40.441, 40.437)
-            latLon[1] = random.uniform(-3.687, -3.691)
-            _rssi = random.uniform(0,1)
-            point = Ping(lat=latLon[0], lon=latLon[1], rssi=_rssi, snr=0, id=int(time.time()))
-            db.session.add(point)
-            db.session.commit()
-            print(latLon)
-            socketio.emit('newcoord', {'lat': latLon[0], 'lon': latLon[1], 'rssi': _rssi}, namespace='/test')
-            time.sleep(self.delay)
-
-    def run(self):
-        self.randomNumberGenerator()
-""" 
+Clase que maneja el módem SX1276
+Hereda de la clase LoRa de la biblioteca SX127x
+"""
 class mylora(LoRa):
+    #Método __init__ redefinido para que cuando se instancie un objeto mylora se configure automáticamente
+    #Configuración de canal, coding rate, ancho de banda y spreading factor igual que la placa transmisora
+    #Sólo se usa la capa LoRa, no la LoRaWAN, los mensajes son broadcast en el canal
     def __init__(self, verbose=False):
         super(mylora, self).__init__(verbose)
-        self.set_mode(MODE.SLEEP)
+        self.set_freq(868.1)
         self.set_dio_mapping([0] * 6)
         self.received_new=0
         self.latLon =[]
@@ -77,6 +61,9 @@ class mylora(LoRa):
         self.set_low_data_rate_optim(True)
         assert(self.get_agc_auto_on() == 1)
 
+    #Método on_rx_done de la clase LoRa redefinido
+    #Imprime el último mensaje recibido por consola y sube la bandera received_new de nuevo mensaje
+    #Esta biblioteca maneja, además de la interfaz SPI, otras salidas del módem que alertan de finalización de transmisión, p. ej.
     def on_rx_done(self):
         self.clear_irq_flags(RxDone=1)
         self.payload = self.read_payload(nocheck=True)
@@ -84,7 +71,7 @@ class mylora(LoRa):
         print(bytes(self.payload).decode("utf-8",'ignore')) # Receive DATA
         self.received_new = 1
 
-    
+    #Devuelve el último mensaje y baja la bandera de nuevo mensaje recibido
     def get_last_message(self):
         self.received_new = 0
         return self.payload
@@ -95,33 +82,47 @@ class mylora(LoRa):
     def msg_ready(self):
         return self.received_new
  
-    
+"""
+Hilo que se ejecuta continuamente en background
+Consulta periódicamente si hay mensaje nuevo recibido por LoRa
+"""
 class LoRaThread(Thread):
     def __init__(self):
         self.delay = 1
         super(LoRaThread, self).__init__()
-        self.lora = mylora(verbose=False)
+        self.lora = mylora(verbose=False) #Instancia un objeto mylora que maneja el módem
 
     def loraListener(self):
-        print("Making random numbers")
+        print("LoRa thread")
         while not thread_stop_event.isSet():
-            if self.lora.msg_ready():
+            if self.lora.msg_ready(): #Si hay mensaje nuevo recogemos su valor
                 payload = self.lora.get_last_message()
-                _lat, _lon = struct.unpack('ff', payload)
+                _lat, _lon = struct.unpack('ff', payload) #Son dos floats empaquetados como 4+4 bytes, los extraemos
                 _rssi = self.lora.get_pkt_rssi_value()
                 _snr = self.lora.get_pkt_snr_value()
                 print("lat: ")
                 print(_lat)
                 print(" lon: ")
                 print("\r\n")
+                #Emitimos por el socket /test un objeto newcoord con la información del nuevo paquete
+                #Se recibe dinámicamente en el navegador
                 socketio.emit('newcoord', {'lat': float(_lat), 'lon': float(_lon), 'rssi': _rssi, 'snr': _snr}, namespace='/test')
+                #Se guarda el punto obtenido en la base de datos
+                point = Ping(id=int(time.time()), lat=_lat, lon=_lon, rssi=_rssi, snr=_snr)
+                db.session.add(point)
+                db.session.commit()
+                #Se duerme el hilo hasta la siguiente activación
                 time.sleep(self.delay)
 
     def run(self):
         self.loraListener()
 
 
-
+"""
+Modelo de Ping en la base de datos
+Cada paquete que se reciba se guarda en forma de una entrada con esta estructura
+Definido pero no se ha implementado el manejo
+"""
 class Ping(db.Model):
     __table_args__ = {'sqlite_autoincrement': True}
     id = db.Column(db.Integer, primary_key=True)
@@ -141,18 +142,28 @@ class Ping(db.Model):
     def strength(self):
         return 0
 
-
+"""
+Función que devuelve la página principal cuando se accede a la raíz del servidor
+El aspecto de la página depende de la plantilla index.html
+Cuando se accede aquí se abre el socket
+"""
 @app.route('/')
 def index():
-    #only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
 
+"""
+Función que devuelve todos los puntos guardados en la base de datos en forma de json
+No implementado aún lo que se hará con ellos
+"""
 @app.route('/getpoints')
 def getpoints(district_id):
     points = Point.query.all()
     coords = [[point.lat, point.lon, point.rssi] for point in points]
     return jsonify({"data": coords})
 
+"""
+Cuando se conecte el navegador a la ruta /test se arranca el hilo del módem
+"""
 @socketio.on('connect', namespace='/test')
 def test_connect():
     # need visibility of the global thread object
@@ -165,9 +176,20 @@ def test_connect():
         thread = RandomThread()
         thread.start()
 
+"""
+Aviso de desconexión del cliente
+"""
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
+
+#Función para desconectar el módem y cerrar el hilo que lo maneja al cerrar el programa
+def closing_function():
+    LoRaThread.lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
+    thread.join()
+#Register the function to be called on exit
+atexit.register(closing_function)
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/lora.js
+++ static/lora.js
@@ -1,6 +1,6 @@
-BASECOORDS = [40.440283, -3.689095];
+BASECOORDS = [40.440283, -3.689095]; //Centro del mapa en el CEI
 
-function makeMap() {
+function makeMap() { //Función estándar de inicializado de mapa de la biblioteca Leaflet
     var TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
     var MB_ATTR = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
     mymap = L.map('llmap').setView(BASECOORDS, 8);
@@ -10,17 +10,17 @@ function makeMap() {
 
 
 
-$(document).ready(function(){
+$(document).ready(function(){//Función de JQuery
    
-    makeMap();
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    makeMap(); //Inicializa el mapa
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test'); //Se conecta al socket
     var layer = L.layerGroup();
-    var heat = L.heatLayer([], {radius: 25}).addTo(mymap);
-    //receive details from server
+    var heat = L.heatLayer([], {radius: 25}).addTo(mymap); //Se crea la capa del heatmap
+    //Recibe mensajes del servidor
     socket.on('newcoord', function(msg) {
-        console.log("lat: " + msg.lat + " lon: " + msg.lon + " rssi: " + msg.rssi);
+        console.log("lat: " + msg.lat + " lon: " + msg.lon + " rssi: " + msg.rssi); //Saca el contenido del paquete por consola de debug
         //maintain a list of ten numbers
-        heat.addLatLng(L.latLng(msg.lat, msg.lon, msg.rssi))
+        heat.addLatLng(L.latLng(msg.lat, msg.lon, msg.rssi)) //Se añade el punto al heatmap
     });
 
 })
